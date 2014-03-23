56c44f3c5f7cbeadeb7f2a4e2aeaf5e23377707c
Gabriel Reitz Giannattasio <gartz@l6.com.br>
2014-03-23 06:21:19 -0300

    Files cleanup

--- index.js
+++
@@ -1,8 +0,0 @@
-/**
- * pushStateTree
- * https://github.com/gartz/pushStateTree/
- * Copyright (c) 2014 Gabriel Reitz Giannattasio
- * Licensed under the MIT license.
- */
-
-exports = module.exports = require('lib/pushStateTree');
