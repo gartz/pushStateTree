cda237ccbc218ca2b0c2ea5601a646c22e7762df
Gabriel Reitz Giannattasio <gartz@l6.com.br>
2014-09-07 15:14:55 -0700

    Added node-static to create a simple pushState development server for testing on port 3000

---
+++ server.js
@@ -0,0 +1,21 @@
+// This is a simple static server for dev
+// Just start it to you can test your libs
+// in a browser using HTTP protocol on port 3000
+
+var static = require('node-static');
+
+var fileServer = new static.Server('./');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response, function (e){
+            if (e && (e.status === 404)) { // If the file wasn't found
+                fileServer.serveFile('/index.html', 200, {}, request, response);
+            }
+        });
+    }).resume();
+}).listen(3000, function (){
+    console.log('Open development server at port 3000');
+    console.log('Files not found will load /index.html for pushState works!');
+    console.log('http://localhost:3000');
+});
