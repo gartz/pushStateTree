e9a72511d110170ab70230fd38c13d5857527d1a
Gabriel Reitz Giannattasio <gartz@l6.com.br>
2016-04-01 17:05:14 -0700

    Fix identation

--- src/customEvent.shim.js
+++ src/customEvent.shim.js
@@ -14,10 +14,10 @@ if (typeof PST_NO_SHIM === 'undefined') {
 
     function CustomEvent(event, params) {
       params = params || {
-          bubbles: false,
-          cancelable: false,
-          detail: undefined
-        };
+        bubbles: false,
+        cancelable: false,
+        detail: undefined
+      };
       var evt = document.createEvent('CustomEvent');
       evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
       return evt;
