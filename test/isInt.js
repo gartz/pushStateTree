05108f60417154a191f30dcba41475152389499d
tonho <mussiantonio@gmail.com>
2016-04-04 13:31:50 -0300

    added a test for isInt(null)

--- test/isInt.js
+++ test/isInt.js
@@ -37,4 +37,8 @@ describe('PushStateTree isInt', function() {
   it('should return false for strings with letters', function() {
     expect(PushStateTree.isInt('1bc')).to.be.false;
   });
+
+  it('should return false for null', function() {
+    expect(PushStateTree.isInt(null)).to.be.false;
+  });
 });
