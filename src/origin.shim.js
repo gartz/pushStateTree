25f4304ae2ade16b477233c43c5e26722814fc7c
Gabriel Reitz Giannattasio <gartz@l6.com.br>
2016-04-07 08:51:13 -0700

    Removed origin shim

--- src/origin.shim.js
+++
@@ -1,8 +0,0 @@
-if (typeof PST_NO_SHIM === 'undefined') {
-  // Opera and most of IE version doesn't implement location.origin
-  // https://developer.mozilla.org/en-US/docs/Web/API/Window/location
-  let root = window;
-  if (root && !root.location.origin) {
-    root.location.origin = root.location.protocol + '//' + root.location.host;
-  }
-}
