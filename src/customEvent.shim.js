2d2ca68ec48a644aade61e4ce0aa8b3a524c8518
Gabriel Reitz Giannattasio <gartz@l6.com.br>
2016-04-08 22:35:27 -0700

    Fix lint issues

--- src/customEvent.shim.js
+++ src/customEvent.shim.js
@@ -13,7 +13,7 @@ if (typeof PST_NO_SHIM === 'undefined') {
     throw new Error('CustomEvent shim needs EventTarget support to work.');
   }
 
-  function CustomEvent(event, params) {
+  let CustomEvent = function CustomEvent(event, params) {
     params = params || {
       bubbles: false,
       cancelable: false,
@@ -22,7 +22,7 @@ if (typeof PST_NO_SHIM === 'undefined') {
     var evt = document.createEvent('CustomEvent');
     evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
     return evt;
-  }
+  };
 
   CustomEvent.prototype = Event.prototype;
 
@@ -31,7 +31,7 @@ if (typeof PST_NO_SHIM === 'undefined') {
   }
 
   // Opera before 15 has HashChangeEvent but throw a DOM Implement error
-  if (!root.HashChangeEvent || (root.opera && root.opera.version() < 15) || isIE()) {
+  if (!HashChangeEvent || (root.opera && root.opera.version() < 15) || isIE()) {
     root.HashChangeEvent = root.CustomEvent;
   }
 
@@ -41,13 +41,13 @@ if (typeof PST_NO_SHIM === 'undefined') {
 
   // fix for Safari
   try {
-    new root.HashChangeEvent('hashchange');
+    new HashChangeEvent('hashchange');
   } catch (e) {
     root.HashChangeEvent = CustomEvent;
   }
 
   try {
-    new root.Event('popstate');
+    new Event('popstate');
   } catch (e) {
     root.Event = CustomEvent;
   }
