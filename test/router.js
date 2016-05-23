df701c2997c6949c4eeac48813e20045b1b3108c
Gabriel Reitz Giannattasio <gartz@l6.com.br>
2016-05-23 08:58:47 -0700

    Remove tests related to the history API from the router

--- test/router.js
+++ test/router.js
@@ -1,9 +1,6 @@
-const PushStateTree = require('../src/main');
-import cleanHistoryAPI from './helper/cleanHistoryAPI';
+import PushStateTree from '../src/push-state-tree';
 
 describe('PushStateTree should', function() {
-  cleanHistoryAPI();
-
   it('be available as UMD library', function() {
     expect(PushStateTree).to.be.defined;
   });
@@ -17,51 +14,4 @@ describe('PushStateTree should', function() {
     expect(new PushStateTree()).to.be.instanceof(HTMLElement);
     expect(new PushStateTree({})).to.be.instanceof(HTMLElement);
   });
-
-  it('auto enable push state if browser support it', function(){
-    var pst = new PushStateTree();
-    expect(pst.usePushState).to.be.true;
-  });
-
-  it('allow to disable push state if it constructor has a false option', function(){
-    var pst = new PushStateTree({
-      usePushState: false
-    });
-    expect(pst.usePushState).to.be.false;
-  });
-
-  it('allow to enable push state if it constructor has a true option', function(){
-    var pst = new PushStateTree({
-      usePushState: true
-    });
-    expect(pst.usePushState).to.be.true;
-  });
-
-  it('usePushState be true by default', function(){
-    var pst = new PushStateTree();
-    expect(pst.usePushState).to.be.true;
-  });
-
-  it('allow to change the usePushState flag after start running', function(){
-    var pst = new PushStateTree();
-    pst.usePushState = false;
-    expect(pst.usePushState).to.be.false;
-    pst.usePushState = true;
-    expect(pst.usePushState).to.be.true;
-  });
-
-  it('usePushState change for each instance', function(){
-    var pst1 = new PushStateTree({
-      usePushState: false
-    });
-    var pst2 = new PushStateTree();
-    expect(pst1.usePushState).to.be.false;
-    expect(pst2.usePushState).to.be.true;
-  });
-
-  it('get the current URI', function(){
-    var pst = new PushStateTree();
-    expect(pst.uri).to.equal(location.pathname.slice(1));
-  });
-
 });
